e80398a48c70594000174dd0367136221c693135
razcohen <raz@icohens.com>
2021-06-12 17:11:42 +0300

    some missing files

---
+++ app/dependency-stack/src/test/java/com/collaborations/dependencystack/DependencyStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collaborations.dependencystack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
